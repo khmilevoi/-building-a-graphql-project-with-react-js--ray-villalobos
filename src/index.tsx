0b64bad157c5c0a64f1ced107e91ac96c0023f7e
khmilevoi <khmilevoi@gmail.com>
2022-01-21 16:14:06 +0200

    Configure typescript

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { App } from "App";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
